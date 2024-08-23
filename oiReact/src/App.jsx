175c54048a5c8ed46ee8afe1326cda869b023faa
evylohanny <evelyn_l_silva@estudante.sesisenai.org.br>
2024-08-23 17:16:15 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Dinossauros</h1>
+      <h2>Carnívoros</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod commodi qui voluptatum repellendus, dolore possimus molestiae reprehenderit id veritatis sit quisquam, accusantium hic provident. Suscipit aspernatur iusto impedit in! Atque.</p>
+    </>
+  )
+}
+
+export default App
